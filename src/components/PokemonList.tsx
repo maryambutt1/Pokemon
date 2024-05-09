be09422a4e37a387456ad74fa3ef889818a1387c
maryambutt1 <maryam.butt@devsinc.com>
2024-05-09 20:14:33 +0500

    fetch-all-pokemons-and-pagination

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -4,47 +4,73 @@ import "./PokemonList.css";
 
 const PokemonList: React.FC = () => {
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/pokemon/fetch")
-      .then((response) => response.json())
-      .then((data) => {
-        // Ensure data is an array of Pokemon objects
-        if (Array.isArray(data)) {
-          // Update the state with the fetched Pokémon data
-          setPokemonList(data);
-        } else {
-          console.error("Data received is not an array:", data);
-        }
-      })
-      .catch((error) => console.error(error));
-  }, []);
+    const fetchPokemonData = async () => {
+      setLoading(true); // Set loading to true when fetching data
+      try {
+        const response = await fetch(
+          `http://localhost:5000/api/pokemon/fetch?page=${currentPage}`
+        );
+        const data = await response.json();
+        setPokemonList(data);
+        setLoading(false); // Set loading to false after data is fetched
+      } catch (error) {
+        console.error(error);
+        setLoading(false); // Set loading to false if there's an error
+      }
+    };
 
+    fetchPokemonData();
+  }, [currentPage]);
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
   return (
     <div>
       <h1>Pokémon List</h1>
       <div>
-        <ul className="pokemon-list">
-          {pokemonList.map((pokemon: Pokemon, index: number) => (
-            <li key={index}>
-              <Link
-                className="pokemon-list-item"
-                to={`/pokemon/${pokemon.name}`}
-              >
-                <img
-                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                    index + 1
-                  }.png`}
-                  alt={pokemon.name}
-                />
-                <span>{pokemon.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div>
+            <ul className="pokemon-list">
+              {pokemonList.map((pokemon: Pokemon, index: number) => (
+                <li key={index}>
+                  <Link
+                    className="pokemon-list-item"
+                    to={`/pokemon/${pokemon.name}`}
+                  >
+                    <img
+                      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                        index + 1
+                      }.png`}
+                      alt={pokemon.name}
+                    />
+                    <span>{pokemon.name}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+
+            <div className="pagination">
+              <button onClick={handlePrevPage}>Previous</button>
+              <span>{currentPage}</span>
+              <button className="next-btn" onClick={handleNextPage}>Next</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
 };
-
 export default PokemonList;
